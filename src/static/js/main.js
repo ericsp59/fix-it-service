6d4a9ccada9ec239542593df776dc1c6c27749fd
ericsp <ericsp5900@gmail.com>
2019-04-18 17:29:00 +0500

    Хочу переделать why-we, готово buisness

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -3,39 +3,39 @@ $(document).ready(function(){
 
     ////////////////////////////////////////////////////
 
-    function randomInteger(min, max) {
-        var rand = min + Math.random() * (max - min)
-        rand = Math.round(rand);
-        return rand;
-    }
-//////////////////////////////////////////////////////
-    function makeCounter(num){
-        var count = 0;
-        return function(){
-           while ( count < num ){
-               return count++
-           }
-           count = 0
-           return 1
-        };    
-    }
-    var counter = makeCounter(3);
-/////////////////////////////////////////////////////////////
+//     function randomInteger(min, max) {
+//         var rand = min + Math.random() * (max - min)
+//         rand = Math.round(rand);
+//         return rand;
+//     }
+// //////////////////////////////////////////////////////
+//     function makeCounter(num){
+//         var count = 0;
+//         return function(){
+//            while ( count < num ){
+//                return count++
+//            }
+//            count = 0
+//            return 1
+//         };    
+//     }
+//     var counter = makeCounter(3);
+// /////////////////////////////////////////////////////////////
 
-    function changeWallpapers() {
-        var walls = [
-            "background1.png",
-            "background2.png",
-            "background3.png"
-        ];
+//     function changeWallpapers() {
+//         var walls = [
+//             "background1.png",
+//             "background2.png",
+//             "background3.png"
+//         ];
 
-        $(".features")
-        .css({"background":"url('http://localhost:3000/static/img/" + walls[counter()] + "')no-repeat"})
-        .animate({ 'background-size':'110%' }, 0)
-        .animate({ 'background-size':'130%' }, 12000, function(){})
-    }
-     changeWallpapers();
-    var intervalCsaHead = setInterval(changeWallpapers, 12000);
+//         $(".features")
+//         .css({"background":"url('http://localhost:3000/static/img/" + walls[counter()] + "')no-repeat"})
+//         .animate({ 'background-size':'120%' }, 0)
+//         .animate({ 'background-size':'130%' }, 12000, function(){})
+//     }
+//      changeWallpapers();
+//     var intervalCsaHead = setInterval(changeWallpapers, 12000);
 
 
 

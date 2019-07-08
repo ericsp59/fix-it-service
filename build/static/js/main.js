8657fb7039d17af207e5a4f6a9e6d96d7798a7f5
ericsp <ericsp5900@gmail.com>
2019-07-08 17:22:57 +0500

    modal window is created

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,46 +1,6 @@
 //main.js 
 $(document).ready(function(){
 
-    ////////////////////////////////////////////////////
-
-//     function randomInteger(min, max) {
-//         var rand = min + Math.random() * (max - min)
-//         rand = Math.round(rand);
-//         return rand;
-//     }
-// //////////////////////////////////////////////////////
-//     function makeCounter(num){
-//         var count = 0;
-//         return function(){
-//            while ( count < num ){
-//                return count++
-//            }
-//            count = 0
-//            return 1
-//         };    
-//     }
-//     var counter = makeCounter(3);
-// /////////////////////////////////////////////////////////////
-
-//     function changeWallpapers() {
-//         var walls = [
-//             "background1.png",
-//             "background2.png",
-//             "background3.png"
-//         ];
-
-//         $(".features")
-//         .css({"background":"url('http://localhost:3000/static/img/" + walls[counter()] + "')no-repeat"})
-//         .animate({ 'background-size':'120%' }, 0)
-//         .animate({ 'background-size':'130%' }, 12000, function(){})
-//     }
-//      changeWallpapers();
-//     var intervalCsaHead = setInterval(changeWallpapers, 12000);
-
-
-
-    ///////////////////////////////////////////////////////////
-
     $('.multiple-items').slick({
         autoplay: true,
         autoplaySpeed: 8000,
@@ -67,5 +27,12 @@ $(document).ready(function(){
         fade: true,
         appendArrows: ('.arrows')
     });
-    
+
+
+    $('.modalCallback').click(function(){
+        console.log( $('#modal')[0].style.display = '' )
+    })
+    $('#closeModal').click(function(){
+        console.log( $('#modal')[0].style.display = 'none' )
+    })
 });
